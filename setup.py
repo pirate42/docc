4a023b9272111c2210670f7e7a9f1e06aaee07fd
David Segonds <david@segonds.org>
2013-04-14 15:00:17 -0500

    Bumping up the version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Docc',
-    version='0.0.1',
+    version='0.0.2',
     packages=['docc', 'tests'],
     description='Digital Ocean Command Center',
     long_description=open('README.txt').read(),
